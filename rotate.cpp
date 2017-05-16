eacf262d6c7a7255ad794943c2b35c71418f2975
alshatub <ahmed.alshatub@yahoo.com>
2017-05-16 21:03:30 +0300

    Update rotate.cpp

--- rotate.cpp
+++ rotate.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
 using namespace std;
-void rotate(int arr[5], int i) {
+void rotate(int arr[6], int i) {
+	int n=6;
 	int j;
-	for (j = 0; j < 5; ++j) {
-		int t = arr[4];
-		for (i = 4; i > j; i--) 
+	for (j = 0; j < n; ++j) {
+		int t = arr[n-1];
+		for (i = n-1; i > j; i--)
 			arr[i] = arr[i - 1];
-		
+
 		arr[i] = 0;
-		for ( i = 0; i < 5; ++i) {
+		for ( i = 0; i < n; ++i) {
 	cout<<arr[i]<<" ";
 }
+		cout<<endl;
 	}
 }
 
 int main() {
 	int i=0;
-int arr[5]={1,2,3,4,5};
-for ( i = 0; i < 5; ++i) {
+	
+int arr[6]={1,2,3,4,5,6};
+for ( i = 0; i < 6; ++i) {
 	cout<<arr[i]<<" ";
 }
 cout<<endl;
 
 
 	rotate(arr, i);
-	
-	
+
+
 //after
-for ( i = 0; i < 5; ++i) {
+for ( i = 0; i < 6; ++i) {
 	cout<<arr[i]<<" ";
 }cout<<endl;
 
